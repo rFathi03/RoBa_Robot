e7abb7709561b703946ebc043221af0bba5d7436
Basma <92788812+Basma2423@users.noreply.github.com>
2024-03-22 15:04:38 +0200

    sensors uncommented.py

--- project.py
+++ project.py
@@ -9,10 +9,10 @@ MAX_SPEED = 6.28
 robot = Robot()
 
 ps = []
-psNames = ["ps0", "ps1"]
-#  # ()
+psNames = ["Rsensor", "Lsensor"]
+
 for i in range(2):
-    ps.append(getDevice(psNames[i]))
+    ps.append(robot.getDevice(psNames[i]))
     ps[i].enable(TIME_STEP)
 
 front_left_motor = robot.getDevice("LFmotor")
@@ -34,26 +34,26 @@ rear_right_motor.setVelocity(0.0)
 while robot.step(TIME_STEP) != -1:
     # read sensors outputs
     psValues = []
-    for i in range(8):
+    for i in range(2):
         psValues.append(ps[i].getValue())
 
     # detect obstacles
-    right_obstacle = psValues[0] > 80.0
-    left_obstacle = psValues[1] > 80.0
+    right_obstacle = psValues[0] < 1000
+    left_obstacle = psValues[1] < 1000
 
     # initialize motor speeds at 50% of MAX_SPEED.
-    leftSpeed = 0.7 * MAX_SPEED
-    rightSpeed = 0.7 * MAX_SPEED
+    leftSpeed = 0.5 * MAX_SPEED
+    rightSpeed = 0.5 * MAX_SPEED
 
     # modify speeds according to obstacles
     if left_obstacle:
-        # turn right
-        leftSpeed = 0.7 * MAX_SPEED
+        #turn right
+        leftSpeed = 0.5 * MAX_SPEED
         rightSpeed = 0
     elif right_obstacle:
-        # turn left
+        #turn left
         leftSpeed = 0
-        rightSpeed = 0.7 * MAX_SPEED
+        rightSpeed = 0.5 * MAX_SPEED
 
     # write actuators inputs
 
